43b9d234a13879c8b18257fe2f0b56c285e24675
Nqchoz <joshpham15@gmail.com>
2024-07-13 20:53:45 -0700

    Camera changes
    Revamped the camera system to open immediately upon startup. Maintained gallery image selection functionality. However, the aspect ratio of the photo from the camera on the edit page needs to be fixed.

--- lib/input_screen.dart
+++ lib/input_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:io';
+import 'package:camera/camera.dart';
 import 'package:climbingapp/analyzing_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,7 +15,13 @@ class InputScreen extends StatefulWidget {
 
 class _InputScreenState extends State<InputScreen> {
   File? image;
-
+  List<CameraDescription> cameras = [];
+  CameraController? cameraController;
+  @override
+  void initState(){
+    super.initState();
+    _setupCameraController();
+  }
   Future pickImage(ImageSource source) async{ //function that deals with selecting an image
     final pickedImage = await ImagePicker().pickImage(source: source);
     if (pickedImage ==null) return;
@@ -31,28 +40,83 @@ class _InputScreenState extends State<InputScreen> {
     });
     
   }
+  Future<void> _setupCameraController() async{ // setup the camera controller (code from https://youtu.be/TrmoRtn5MZA?si=_5XDCKkRsONxJQTu)
+    List <CameraDescription> _cameras = await availableCameras(); //see if there are any cameras on the device
+    if(_cameras.isNotEmpty){
+      setState(() {
+        cameras = _cameras;
+        cameraController=CameraController(_cameras.last, ResolutionPreset.high);
+      });
+      cameraController?.initialize().then((_){
+        setState(() {});
+      });
+    }
+  }
+
+  Widget _buildUI(){
+    if (cameraController == null || cameraController?.value.isInitialized == false){
+      return const Center(child: CircularProgressIndicator(),);
+    }
+    return SafeArea(child: SizedBox.expand(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:[
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.30,
+            width: MediaQuery.sizeOf(context).width * 0.80,
+            child: CameraPreview(
+              cameraController!,
+            ),
+          ),
+          Row(children: [
+            FloatingActionButton.extended(onPressed: (){ //"From Gallery" button
+                pickImage(ImageSource.gallery);
+              }, 
+              label: const Text("From Gallery"), 
+              icon: const Icon(Icons.photo_album_rounded)
+            ),
+
+            FloatingActionButton(onPressed: () async{ //"From Camera" button
+              XFile picture = await cameraController!.takePicture();
+              Navigator.push( //switch screens
+                context,
+                MaterialPageRoute(builder: (context)=> AnalyzingScreen(
+                  img: File(picture.path)
+                  )
+                )
+              );
+              },
+              child: const Icon(Icons.camera_alt_rounded)
+              ),
+            ],
+          )
+          
+
+
+        ]
+      ),
+    ),);
+  }
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
      home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.black87, title: const Text("Climb Sorter (or something idk lol)")),
-        body: Column(
+        body: _buildUI()
+        
+        
+        
+        /*Column(
           children: [
-            FloatingActionButton.extended(onPressed: (){ //"From Gallery" button
-              pickImage(ImageSource.gallery);
-            }, 
-            label: const Text("From Gallery"), 
-            icon: const Icon(Icons.photo_album_rounded)
-            ),
+
+            
              
-            FloatingActionButton.extended(onPressed: (){ //"From Camera" button
-              pickImage(ImageSource.camera); // call pickimage method to get image from gallery
-            }, label: const Text("From Camera"), 
-            icon: const Icon(Icons.camera_alt_rounded)
-            ),
+            
           ]
-        )
+        )*/
      ),
       );
   }
